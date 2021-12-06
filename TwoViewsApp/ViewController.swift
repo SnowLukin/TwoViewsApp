db29fb6926e5e55e30ef0dcbc6b28e335b4ffb6e
Snow Lukin <everyoursden@gmail.com>
2021-12-06 22:50:16 +0300

    Initial Commit

---
+++ TwoViewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViewsApp
+//
+//  Created by Snow Lukin on 06.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
